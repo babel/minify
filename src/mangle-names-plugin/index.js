7c492f8e9190b7a868642f5fd3509a7ca6e12eee
Amjad Masad <amjad.masad@gmail.com>
2015-11-23 22:03:29 -0800

    Simplify the name mangler for consistency

--- src/mangle-names-plugin/index.js
+++ src/mangle-names-plugin/index.js
@@ -1,4 +1,5 @@
-const Base54 = require('./Base54');
+const chars = 'abcdefghijklmnopqrstuvwxyz' +
+              'ABCDEFGHIJKLMNOPQRSTUVWXYZ$'.split('');
 
 module.exports = ({ Plugin, types: t }) => {
   const mangleNamesVisitors = {
@@ -19,7 +20,7 @@ module.exports = ({ Plugin, types: t }) => {
         let newName;
 
         do {
-          newName = this.base54.name(i);
+          newName = getIdentifier(i);
           i += 1;
         } while (!(t.isValidIdentifier(newName)
             && canUse(newName, scope, bindingRefs, this.refs)));
@@ -34,16 +35,13 @@ module.exports = ({ Plugin, types: t }) => {
 
   return {
     pre() {
-      this.base54 = new Base54();
       this.refs = new Map();
     },
 
     visitor: {
       Program: {
         exit(path) {
-          this.base54.sort();
           path.traverse(mangleNamesVisitors, {
-            base54: this.base54,
             refs: this.refs,
           });
         },
@@ -52,99 +50,6 @@ module.exports = ({ Plugin, types: t }) => {
       'ReferencedIdentifier|BindingIdentifier'(path) {
         const { scope, node } = path;
         recordRef(this.refs, scope.getBinding(node.name), path);
-      },
-
-      'FunctionExpression|FunctionDeclaration'(path) {
-        this.base54.consider('function');
-      },
-
-      'ClassExpression|ClassDeclaration'(path) {
-        this.base54.consider('class');
-      },
-
-      Identifier({ scope, node }) {
-        // Don't consider lexical bindings as they will be renamed.
-        if (!scope.getBinding(node.name)) {
-          this.base54.consider(node.name);
-        }
-      },
-
-      ReturnStatement(path) {
-        this.base54.consider('return');
-      },
-
-      ThrowStatement(path) {
-        this.base54.consider('throw');
-      },
-
-      ContinueStatement(path) {
-        this.base54.consider('continue');
-      },
-
-      BreakStatement(path) {
-        this.base54.consider('break');
-      },
-
-      DebuggerStatement(path) {
-        this.base54.consider('debugger');
-      },
-
-      WhileStatement(path) {
-        this.base54.consider('while');
-      },
-
-      DoWhileStatement(path) {
-        this.base54.consider('do while');
-      },
-
-      For(path) {
-        this.base54.consider('for');
-      },
-
-      ForInStatement(path) {
-        this.base54.consider('in');
-      },
-
-      ForOfStatement(path) {
-        this.base54.consider('of');
-      },
-
-      IfStatement({ node }) {
-        this.base54.consider('if');
-        if (node.alternate) {
-          this.base54.consider('else');
-        }
-      },
-
-      VariableDeclaration({ node }) {
-        this.base54.consider(node.kind);
-      },
-
-      NewExpression(path) {
-        this.base54.consider('new');
-      },
-
-      ThisExpression(path) {
-        this.base54.consider('this');
-      },
-
-      TryStatement({ node }) {
-        this.base54.consider('try');
-        if (node.finalizer) {
-          this.base54.consider('finally');
-        }
-      },
-
-      CatchClause(path) {
-        this.base54.consider('catch');
-      },
-
-      Literal({ node }) {
-        this.base54.consider(String(node.value));
-      },
-
-      'UnaryExpression|BinaryExpression'({ node }) {
-        this.base54.consider(node.operator);
       },
     },
   };
@@ -170,14 +75,6 @@ module.exports = ({ Plugin, types: t }) => {
       }
       return true;
     }
-
-    // If any of the references is in a scope with a competing
-    // binding then we need to pick a different name.
-    for (let ref of refs) {
-      if (ref.scope.getBinding(name)) {
-        return false;
-      }
-    }
     return true;
   }
 
@@ -186,5 +83,16 @@ module.exports = ({ Plugin, types: t }) => {
       refs.set(binding, []);
     }
     refs.get(binding).push(refPath);
+  }
+
+  function getIdentifier(num) {
+    let ret = '';
+    num++;
+    do {
+      num--;
+      ret += chars[num % chars.length];
+      num = Math.floor(num / chars.length);
+    } while (num > 0);
+    return ret;
   }
 };
