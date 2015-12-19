982b3124b1edb5f95109f1dc58108e445e341be0
Amjad Masad <amjad.masad@gmail.com>
2015-12-18 19:11:21 -0800

    cli

---
+++ bin/babel-minify.js
@@ -0,0 +1,49 @@
+var code = '';
+
+var replaceOptIndex = process.argv.indexOf('--replace');
+if (replaceOptIndex !== -1) {
+  var replacements = [];
+  process.argv[replaceOptIndex + 1].split(',').forEach(function(record) {
+    var tmp = record.split(':');
+    var id = tmp[0];
+    var repl = tmp[1];
+    var member;
+    if (id.match(/\./)) {
+      tmp = id.split('.');
+      id = tmp[0];
+      member = tmp[1];
+    }
+    var type;
+    if (repl.match(/^\d/)) {
+      type = 'numericLiteral';
+      repl = parseInt(repl, 10);
+      if (isNaN(repl)) {
+        throw new Error('Error parsing number');
+      }
+    } else if (repl.match(/^(true|false)$/)) {
+      type = 'booleanLiteral';
+    } else if (repl.match(/^null$/)) {
+      type = 'nullLiteral';
+    } else {
+      type = 'identifier';
+    }
+
+    replacements.push({
+      identifierName: id,
+      member: member,
+      replacement: {
+        type: type,
+        value: repl,
+      },
+    });
+  });
+}
+
+process.stdin.on('data', function(d) {
+  code += d.toString('utf-8');
+});
+
+process.stdin.on('end', function() {
+  var res = require('../').compile(code, { replacements: replacements });
+  console.log(res.code);
+});
--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 require('babel-jest/node_modules/babel-core').register();
 //Error.stackTraceLimit = Infinity;
 var babel  = require('babel-core');
-var code = '';
-process.stdin.on('data', function(d) {
-  code += d.toString('utf-8');
-});
-process.stdin.on('end', compile);
 
 var plugins = [
   require('./src/dce-plugin'),
@@ -13,58 +8,18 @@ var plugins = [
   require('./src/mangle-names-plugin'),
 ];
 
-var replaceOptIndex = process.argv.indexOf('--replace');
-if (replaceOptIndex !== -1) {
-  var replacements = [];
-  process.argv[replaceOptIndex + 1].split(',').forEach(function(record) {
-    var tmp = record.split(':');
-    var id = tmp[0];
-    var repl = tmp[1];
-    var member;
-    if (id.match(/\./)) {
-      tmp = id.split('.');
-      id = tmp[0];
-      member = tmp[1];
-    }
-    var type;
-    if (repl.match(/^\d/)) {
-      type = 'numericLiteral';
-      repl = parseInt(repl, 10);
-      if (isNaN(repl)) {
-        throw new Error('Error parsing number');
-      }
-    } else if (repl.match(/^(true|false)$/)) {
-      type = 'booleanLiteral';
-    } else if (repl.match(/^null$/)) {
-      type = 'nullLiteral';
-    } else {
-      type = 'identifier';
-    }
-
-    replacements.push({
-      identifierName: id,
-      member: member,
-      replacement: {
-        type: type,
-        value: repl,
-      }
-    });
-  });
-
-  if (replacements.length) {
+exports.compile = function(code, options) {
+  if (options && options.replacements && options.replacements.length) {
     plugins.unshift([
       require('./src/replace-plugin'),
-      { replacements: replacements }
+      { replacements: options.replacements }
     ]);
   }
-}
-
-function compile() {
-  var out = babel.transform(code, {
+  return babel.transform(code, {
     sourceType: 'script',
     plugins: plugins,
     minified: true,
+//    compact: false,
     comments: false,
-  }).code;
-  console.log(out);
+  });
 }
