db6764b5ee25574c02016b30c588dbfa7e5cbd2c
Amjad Masad <amjad.masad@gmail.com>
2016-01-07 15:39:19 -0800

    return should be continue

--- src/dce-plugin/index.js
+++ src/dce-plugin/index.js
@@ -218,7 +218,7 @@ module.exports = ({ Plugin, types: t }) => {
               }
 
               if (bail) {
-                return;
+                continue;
               }
 
               let parent = binding.path.parent;
