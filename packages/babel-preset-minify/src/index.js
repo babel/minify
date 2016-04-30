20834b2ed9dae4cebabce5e4f5d4081a2c48dc3c
James Kyle <me@thejameskyle.com>
2016-04-30 11:31:16 -0700

    Add missing packages to babel-preset-minify

--- packages/babel-preset-minify/src/index.js
+++ packages/babel-preset-minify/src/index.js
@@ -1,9 +1,15 @@
 module.exports = {
   plugins: [
+    require('babel-plugin-minify-booleans'),
+    require('babel-plugin-minify-computed-member-expressions'),
+    require('babel-plugin-minify-computed-properties'),
     require('babel-plugin-minify-constant-folding'),
     require('babel-plugin-minify-dead-code-elimination'),
+    require('babel-plugin-minify-infinity'),
     require('babel-plugin-minify-mangle-names'),
     require('babel-plugin-minify-replace'),
-    require('babel-plugin-minify-simplify')
-  ]
+    require('babel-plugin-minify-simplify'),
+    require('babel-plugin-minify-type-constructors'),
+    require('babel-plugin-minify-undefined'),
+  ],
 };
