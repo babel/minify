a7c7b2287b885b92f917f2454060dd3b64cabb19
Henry Zhu <hi@henryzoo.com>
2016-06-27 15:21:34 -0400

    fixup minify-type-constructors and rm from minify-simplify

--- packages/babel-plugin-minify-simplify/src/index.js
+++ packages/babel-plugin-minify-simplify/src/index.js
@@ -21,32 +21,8 @@ module.exports = ({ Plugin, types: t }) => {
         },
       },
 
-      CallExpression(path) {
-        const { node } = path;
-
-        // Boolean(foo) -> !!foo
-        if (t.isIdentifier(node.callee, { name: 'Boolean' }) &&
-          node.arguments.length === 1 &&
-          !path.scope.getBinding('Boolean')) {
-          path.replaceWith(t.unaryExpression('!', t.unaryExpression('!', node.arguments[0], true), true));
-          return;
-        }
-
-        // Number(foo) -> +foo
-        if (t.isIdentifier(node.callee, { name: 'Number' }) &&
-          node.arguments.length === 1 &&
-          !path.scope.getBinding('Number')) {
-          path.replaceWith(t.unaryExpression('+', node.arguments[0], true));
-          return;
-        }
-
-        // String(foo) -> foo + ''
-        if (t.isIdentifier(node.callee, { name: 'String' }) &&
-          node.arguments.length === 1 &&
-          !path.scope.getBinding('String')) {
-          path.replaceWith(t.binaryExpression('+', node.arguments[0], t.stringLiteral('')));
-          return;
-        }
+      // CallExpression(path) {
+        // const { node } = path;
 
         /* (function() {})() -> !function() {}()
         There is a bug in babel in printing this. Disabling for now.
@@ -62,7 +38,7 @@ module.exports = ({ Plugin, types: t }) => {
           );
           return;
         }*/
-      },
+      // },
 
       UnaryExpression: {
         enter: [
--- packages/babel-plugin-minify-type-constructors/src/index.js
+++ packages/babel-plugin-minify-type-constructors/src/index.js
@@ -6,16 +6,26 @@ module.exports = function({ types: t }) {
       CallExpression(path) {
         const { node } = path;
 
+        // Boolean(foo) -> !!foo
+        if (t.isIdentifier(node.callee, { name: 'Boolean' }) &&
+          node.arguments.length === 1 &&
+          !path.scope.getBinding('Boolean')) {
+          path.replaceWith(t.unaryExpression('!', t.unaryExpression('!', node.arguments[0], true), true));
+          return;
+        }
+
         // Number(foo) -> +foo
         if (t.isIdentifier(node.callee, { name: 'Number' }) &&
-          node.arguments.length === 1) {
+          node.arguments.length === 1 &&
+          !path.scope.getBinding('Number')) {
           path.replaceWith(t.unaryExpression('+', node.arguments[0], true));
           return;
         }
 
         // String(foo) -> foo + ''
         if (t.isIdentifier(node.callee, { name: 'String' }) &&
-          node.arguments.length === 1) {
+          node.arguments.length === 1 &&
+          !path.scope.getBinding('String')) {
           path.replaceWith(t.binaryExpression('+', node.arguments[0], t.stringLiteral('')));
           return;
         }
