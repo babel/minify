3c8bf85fad2e7bb0abc770ced0844bf0d18d9ffb
Henry Zhu <hzoo@users.noreply.github.com>
2016-07-01 11:43:53 -0400

    Add inline-env-vars and node-env-inline (#38)

---
+++ packages/babel-plugin-transform-node-env-inline/__tests__/node-env-inline-test.js
@@ -0,0 +1,23 @@
+jest.autoMockOff();
+
+const babel = require("babel-core");
+const plugin = require("../src/index");
+
+function transform(code) {
+  return babel.transform(code,  {
+    plugins: [plugin],
+  }).code;
+}
+
+describe("node-env-inline-plugin", () => {
+  it("should inline", () => {
+    const prev = process.env.NODE_ENV;
+    process.env.NODE_ENV = "development";
+
+    const source = "process.env.NODE_ENV === \"development\";";
+    const expected = "true;";
+    expect(transform(source)).toBe(expected);
+
+    process.env.NODE_ENV = prev;
+  });
+});
---
+++ packages/babel-plugin-transform-node-env-inline/src/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = function({ types: t }) {
+  return {
+    visitor: {
+      MemberExpression(path) {
+        if (path.matchesPattern("process.env.NODE_ENV")) {
+          path.replaceWith(t.valueToNode(process.env.NODE_ENV));
+
+          if (path.parentPath.isBinaryExpression()) {
+            let evaluated = path.parentPath.evaluate();
+            if (evaluated.confident) {
+              path.parentPath.replaceWith(t.valueToNode(evaluated.value));
+            }
+          }
+        }
+      },
+    },
+  };
+};
