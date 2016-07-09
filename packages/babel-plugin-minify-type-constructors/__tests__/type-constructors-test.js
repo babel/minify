036eb93e2454046b1c78934d795cc0a940b88b1f
Federico Brigante <bfred-it@users.noreply.github.com>
2016-07-10 02:20:25 +0700

    Transform: Array() -> []

--- packages/babel-plugin-minify-type-constructors/__tests__/type-constructors-test.js
+++ packages/babel-plugin-minify-type-constructors/__tests__/type-constructors-test.js
@@ -29,16 +29,88 @@ describe("type-constructors-plugin", () => {
     expect(transform(source)).toBe(expected);
   });
 
-  it("shouldn't change referenced identifiers", () => {
+  it("should turn Array() to []", () => {
+    const source = "Array();";
+    const expected = "[];";
+    expect(transform(source)).toBe(expected);
+  });
+
+  it("should turn new Array() to []", () => {
+    const source = "new Array();";
+    const expected = "[];";
+    expect(transform(source)).toBe(expected);
+  });
+
+  it("should turn Array(nonNumericValue) to [nonNumericValue]", () => {
     const source = unpad(`
-      (function (Boolean, String, Number) {
-        return Boolean(a), String(b), Number(c);
-      })(MyBoolean, MyString, MyNumber);
+      Array("Rome");
+      Array(false);
+      Array(null);
+      Array({});
+      Array([]);
     `);
     const expected = unpad(`
-      (function (Boolean, String, Number) {
-        return Boolean(a), String(b), Number(c);
-      })(MyBoolean, MyString, MyNumber);
+      ["Rome"];
+      [false];
+      [null];
+      [{}];
+      [[]];
+    `);
+    expect(transform(source)).toBe(expected);
+  });
+
+  it("should turn Array(number) to [,] only if number is <=6", () => {
+    const source = unpad(`
+      Array(0);
+      Array(1);
+      Array(6);
+      Array(7);
+    `);
+    const expected = unpad(`
+      [];
+      [,];
+      [,,,,,,];
+      Array(7);
+    `);
+    expect(transform(source)).toBe(expected);
+  });
+
+  it("should turn new Array(number) to Array(number) if number is >6", () => {
+    const source = unpad(`
+      new Array(6);
+      new Array(7);
+    `);
+    const expected = unpad(`
+      [,,,,,,];
+      Array(7);
+    `);
+    expect(transform(source)).toBe(expected);
+  });
+
+  it("should turn Array(value, value) to [value, value]", () => {
+    const source = unpad(`
+      Array("a", "b");
+      new Array("0", "1", {});
+      Array(10, Symbol(), foo());
+    `);
+    const expected = unpad(`
+      ["a", "b"];
+      ["0", "1", {}];
+      [10, Symbol(), foo()];
+    `);
+    expect(transform(source)).toBe(expected);
+  });
+
+  it("shouldn't change referenced identifiers", () => {
+    const source = unpad(`
+      (function (Boolean, String, Number, Array) {
+        return Boolean(a), String(b), Number(c), Array(d);
+      })(MyBoolean, MyString, MyNumber, MyArray);
+    `);
+    const expected = unpad(`
+      (function (Boolean, String, Number, Array) {
+        return Boolean(a), String(b), Number(c), Array(d);
+      })(MyBoolean, MyString, MyNumber, MyArray);
     `);
     expect(transform(source)).toBe(expected);
   });
--- packages/babel-plugin-minify-type-constructors/src/index.js
+++ packages/babel-plugin-minify-type-constructors/src/index.js
@@ -1,5 +1,31 @@
 "use strict";
 
+function replaceArray(t, path) {
+  const { node } = path;
+  if (t.isIdentifier(node.callee, { name: "Array" }) &&
+    !path.scope.getBinding("Array")) {
+
+    // Array(5) -> [,,,,,]
+    if (node.arguments.length === 1 &&
+      typeof node.arguments[0].value === "number") {
+
+      // "Array(7)" is shorter than "[,,,,,,,]"
+      if (node.arguments[0].value <= 6) {
+        path.replaceWith(t.arrayExpression(Array(node.arguments[0].value).fill(null)));
+
+      // new Array(7) -> Array(7)
+      } else if (node.type === "NewExpression") {
+        path.replaceWith(t.callExpression(node.callee, node.arguments));
+      }
+
+    // Array("Hello") -> ["Hello"]
+    } else {
+      path.replaceWith(t.arrayExpression(node.arguments));
+    }
+    return true;
+  }
+}
+
 module.exports = function({ types: t }) {
   return {
     visitor: {
@@ -27,6 +53,17 @@ module.exports = function({ types: t }) {
           node.arguments.length === 1 &&
           !path.scope.getBinding("String")) {
           path.replaceWith(t.binaryExpression("+", node.arguments[0], t.stringLiteral("")));
+          return;
+        }
+
+        // Array() -> []
+        if (replaceArray(t, path)) {
+          return;
+        }
+      },
+      NewExpression(path) {
+        // new Array() -> []
+        if (replaceArray(t, path)) {
           return;
         }
       },
