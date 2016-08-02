181476c79458ecd0f4a0a976b250edfaf9d81c4b
Henry Zhu <hi@henryzoo.com>
2016-08-02 07:34:11 -0400

    Update jest, add test updates from babel/babel

--- packages/babel-plugin-transform-remove-debugger/__tests__/remove-debugger-test.js
+++ packages/babel-plugin-transform-remove-debugger/__tests__/remove-debugger-test.js
@@ -2,6 +2,7 @@ jest.autoMockOff();
 
 const babel = require("babel-core");
 const plugin = require("../src/index");
+const unpad = require("../../../utils/unpad");
 
 function transform(code) {
   return babel.transform(code,  {
@@ -20,5 +21,26 @@ describe("remove-debugger-plugin", () => {
     const source = "debugger; 1;";
     const expected = "1;";
     expect(transform(source)).toBe(expected);
+  });
+
+  it("statement no block", () => {
+    const source = unpad(`
+      if (blah) debugger;
+      for (;;) debugger;
+      for (var blah in []) debugger;
+      for (var blah of []) debugger;
+      while (blah) debugger;
+      do debugger; while (blah);
+    `);
+
+    const expected = unpad(`
+      if (blah) {}
+      for (;;) {}
+      for (var blah in []) {}
+      for (var blah of []) {}
+      while (blah) {}
+      do {} while (blah);
+    `);
+    expect(transform(source).trim()).toBe(expected);
   });
 });
