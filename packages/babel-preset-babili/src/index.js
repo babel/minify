dc3b83fb0aca6110e0af99752d39140441fb880d
Shine Wang <shine.sw.wang@gmail.com>
2016-10-20 23:37:57 -0700

    Adds babel-plugin-transform-regexp-constructors to preset. (#211)

--- packages/babel-preset-babili/src/index.js
+++ packages/babel-preset-babili/src/index.js
@@ -14,6 +14,7 @@ module.exports = {
     require("babel-plugin-transform-merge-sibling-variables"),
     require("babel-plugin-transform-minify-booleans"),
     require("babel-plugin-transform-property-literals"),
+    require("babel-plugin-transform-regexp-constructors"),
     require("babel-plugin-transform-simplify-comparison-operators"),
     require("babel-plugin-transform-undefined-to-void"),
   ],
