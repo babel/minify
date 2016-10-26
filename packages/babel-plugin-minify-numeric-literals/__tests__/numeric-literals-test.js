9c5056958d14e7fcca4837cae84e68925590f1b2
Juriy Zaytsev <kangax@fb.com>
2016-10-26 14:57:10 -0400

    Add more test cases

--- packages/babel-plugin-minify-numeric-literals/__tests__/numeric-literals-test.js
+++ packages/babel-plugin-minify-numeric-literals/__tests__/numeric-literals-test.js
@@ -12,14 +12,27 @@ function transform(code) {
 
 describe("numeric-literals", () => {
   it("should shorten properly", () => {
-    const source = unpad(`
-      [10, 100, 1000, 10000, -2, -30000, 1e3, -1e4, 1e-5, 1.5e12, 1.23456, .1]
-    `);
 
-    const expected = unpad(`
-      [10, 100, 1e3, 1e4, -2, -3e4, 1e3, -1e4, 1e-5, 1.5e12, 1.23456, .1];
-    `);
-    
+    let source = '[10, 100, 1000, 10000, -2, -30000];';
+    let expected = '[10, 100, 1e3, 1e4, -2, -3e4];'
+
+    expect(transform(source)).toBe(expected);
+
+    source = '[1e3, -1e4, 1e-5, 1.5e12, 1.23456, .1];';
+    expected = '[1e3, -1e4, 1e-5, 1.5e12, 1.23456, .1];';
+
+    expect(transform(source)).toBe(expected);
+
+    source = '[0x000001, 0o23420, 0b10011100010000];';
+    expected = '[1, 1e4, 1e4];';
+
+    expect(transform(source)).toBe(expected);
+
+    source = '[+0.000000000001, -0.00000000001];';
+    // TODO: this seems to be specific to how Babel outputs number
+    // for some reason it adds + in the beginning
+    expected = '[+1e-12, -1e-11];';
+
     expect(transform(source)).toBe(expected);
   });
 });
