ba7741d4e21683185df5249c7f4e7cc306301e96
Boopathi Rajaa <me@boopathi.in>
2016-10-26 22:07:31 +0200

    Lint fixes for numerical literals

--- packages/babel-plugin-minify-numeric-literals/__tests__/numeric-literals-test.js
+++ packages/babel-plugin-minify-numeric-literals/__tests__/numeric-literals-test.js
@@ -2,7 +2,6 @@ jest.autoMockOff();
 
 const babel = require("babel-core");
 const plugin = require("../src/index");
-const unpad = require("../../../utils/unpad");
 
 function transform(code) {
   return babel.transform(code,  {
@@ -13,25 +12,25 @@ function transform(code) {
 describe("numeric-literals", () => {
   it("should shorten properly", () => {
 
-    let source = '[10, 100, 1000, 10000, -2, -30000];';
-    let expected = '[10, 100, 1e3, 1e4, -2, -3e4];'
+    let source = "[10, 100, 1000, 10000, -2, -30000];";
+    let expected = "[10, 100, 1e3, 1e4, -2, -3e4];";
 
     expect(transform(source)).toBe(expected);
 
-    source = '[1e3, -1e4, 1e-5, 1.5e12, 1.23456, .1];';
-    expected = '[1e3, -1e4, 1e-5, 1.5e12, 1.23456, .1];';
+    source = "[1e3, -1e4, 1e-5, 1.5e12, 1.23456, .1];";
+    expected = "[1e3, -1e4, 1e-5, 1.5e12, 1.23456, .1];";
 
     expect(transform(source)).toBe(expected);
 
-    source = '[0x000001, 0o23420, 0b10011100010000];';
-    expected = '[1, 1e4, 1e4];';
+    source = "[0x000001, 0o23420, 0b10011100010000];";
+    expected = "[1, 1e4, 1e4];";
 
     expect(transform(source)).toBe(expected);
 
-    source = '[+0.000000000001, -0.00000000001];';
+    source = "[+0.000000000001, -0.00000000001];";
     // TODO: this seems to be specific to how Babel outputs number
     // for some reason it adds + in the beginning
-    expected = '[+1e-12, -1e-11];';
+    expected = "[+1e-12, -1e-11];";
 
     expect(transform(source)).toBe(expected);
   });
