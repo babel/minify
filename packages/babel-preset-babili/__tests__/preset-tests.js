ca7df3db523ef012709629c5f1a2fc41c0eeb352
Boopathi Rajaa <me@boopathi.in>
2016-11-03 18:32:01 +0100

    Fix preset tests

--- packages/babel-preset-babili/__tests__/preset-tests.js
+++ packages/babel-preset-babili/__tests__/preset-tests.js
@@ -33,8 +33,8 @@ describe("preset", () => {
     `);
     const expected = unpad(`
       function foo() {
-        var d, e, f;
-        d ? e && f : !b && f;
+        var d, a, b;
+        d ? a && b : !b && b;
       }
     `);
     expect(transform(source)).toBe(expected);
