319d69dc74ce9d801afd5e0f8e4c5c201b20dfa1
Juriy Zaytsev <kangax@gmail.com>
2016-11-13 12:26:53 -0800

    Shorten assignments to itself (#230)
    * Simplify assignments to same identifier
    
    * Add tests
    
    * Bump versions
    
    * Remove versioning
    
    * Remove version
    
    * Add support for optimizing member expressions
    
    * Fix test name
    
    * Remove comment
    
    * Simplify ternary
    
    * Fix bugs, simplify check
    
    * Change postfix to prefix
    
    * Exclude operators for member expressions too
    
    * includes -> indexOf
    
    * Fix lint
    
    * Improve method name

--- packages/babel-plugin-minify-simplify/src/index.js
+++ packages/babel-plugin-minify-simplify/src/index.js
@@ -28,6 +28,62 @@ module.exports = ({ types: t }) => {
   const or = (a, b) => t.logicalExpression("||", a, b);
   const and = (a, b) => t.logicalExpression("&&", a, b);
 
+  const operators = new Set([
+    "+", "-", "*", "%",
+    "<<", ">>", ">>>",
+    "&", "|", "^", "/",
+    "**"
+  ]);
+
+  const updateOperators = new Set([
+    "+", "-"
+  ]);
+
+  function areArraysEqual(arr1, arr2) {
+    return arr1.every((value, index) => {
+      return String(value) === String(arr2[index]);
+    });
+  }
+
+  function getName(node) {
+    if (node.type === "ThisExpression") {
+      return "this";
+    }
+    if (node.type === "Super") {
+      return "super";
+    }
+    if (node.type === "NullLiteral") {
+      return "null";
+    }
+    // augment identifiers so that they don't match
+    // string/number literals
+    // but still match against each other
+    return node.name
+      ? node.name + "_"
+      : node.value /* Literal */;
+  }
+
+  function getPropNames(path) {
+    if (!path.isMemberExpression()) {
+      return;
+    }
+
+    let obj = path.get("object");
+
+    const prop = path.get("property");
+    const propNames = [getName(prop.node)];
+
+    while (obj.type === "MemberExpression") {
+      const node = obj.get("property").node;
+      if (node) {
+        propNames.push(getName(node));
+      }
+      obj = obj.get("object");
+    }
+    propNames.push(getName(obj.node));
+
+    return propNames;
+  }
   const OP_AND = (input) => input === "&&";
   const OP_OR = (input) => input === "||";
 
@@ -192,6 +248,57 @@ module.exports = ({ types: t }) => {
             path.replaceWith(result.value(t.valueToNode(value), right.node));
           }
         }
+      },
+
+      AssignmentExpression(path) {
+
+        const rightExpr = path.get("right");
+        const leftExpr = path.get("left");
+
+        const canBeUpdateExpression = (
+          rightExpr.get("right").isNumericLiteral() &&
+          rightExpr.get("right").node.value === 1 &&
+          updateOperators.has(rightExpr.node.operator));
+
+        if (leftExpr.isMemberExpression()) {
+
+          const leftPropNames = getPropNames(leftExpr);
+          const rightPropNames = getPropNames(rightExpr.get("left"));
+
+          if (!leftPropNames ||
+              leftPropNames.indexOf(undefined) > -1 ||
+              !rightPropNames ||
+              rightPropNames.indexOf(undefined) > -1 ||
+              !operators.has(rightExpr.node.operator) ||
+              !areArraysEqual(leftPropNames, rightPropNames)) {
+            return;
+          }
+        }
+        else {
+          if (!rightExpr.isBinaryExpression() ||
+              !operators.has(rightExpr.node.operator) ||
+              leftExpr.node.name !== rightExpr.node.left.name) {
+            return;
+          }
+        }
+
+        let newExpression;
+
+        // special case x=x+1 --> ++x
+        if (canBeUpdateExpression) {
+          newExpression = t.updateExpression(
+            rightExpr.node.operator + rightExpr.node.operator,
+            t.clone(leftExpr.node),
+            true /* prefix */);
+        }
+        else {
+          newExpression = t.assignmentExpression(
+            rightExpr.node.operator + "=",
+            t.clone(leftExpr.node),
+            t.clone(rightExpr.node.right));
+        }
+
+        path.replaceWith(newExpression);
       },
 
       ConditionalExpression: {
