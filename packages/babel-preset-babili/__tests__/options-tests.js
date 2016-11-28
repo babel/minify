25985c96bdb73112b55427a84093cf54863ef82e
Shine Wang <shinew@fb.com>
2016-11-28 11:27:34 -0800

    added to preset

--- packages/babel-preset-babili/__tests__/options-tests.js
+++ packages/babel-preset-babili/__tests__/options-tests.js
@@ -1,26 +1,27 @@
 jest.autoMockOff();
 
 const mocks = [
+  "babel-plugin-inline-consecutive-adds",
   "babel-plugin-minify-constant-folding",
   "babel-plugin-minify-dead-code-elimination",
   "babel-plugin-minify-flip-comparisons",
-  "babel-plugin-transform-simplify-comparison-operators",
   "babel-plugin-minify-guarded-expressions",
-  "babel-plugin-minify-type-constructors",
   "babel-plugin-minify-infinity",
   "babel-plugin-minify-mangle-names",
   "babel-plugin-minify-numeric-literals",
   "babel-plugin-minify-replace",
   "babel-plugin-minify-simplify",
+  "babel-plugin-minify-type-constructors",
   "babel-plugin-transform-member-expression-literals",
-  "babel-plugin-transform-property-literals",
   "babel-plugin-transform-merge-sibling-variables",
   "babel-plugin-transform-minify-booleans",
-  "babel-plugin-transform-undefined-to-void",
+  "babel-plugin-transform-property-literals",
   "babel-plugin-transform-regexp-constructors",
-  "babel-plugin-transform-remove-debugger",
   "babel-plugin-transform-remove-console",
+  "babel-plugin-transform-remove-debugger",
   "babel-plugin-transform-remove-undefined",
+  "babel-plugin-transform-simplify-comparison-operators",
+  "babel-plugin-transform-undefined-to-void",
 ];
 
 mocks.forEach((mockName) => {
