ea3c9c9c1d3c420906f6e150c1078a1a6e7fc2b6
Juriy Zaytsev <kangax@fb.com>
2016-12-21 15:15:02 -0500

    Reorganize harness so that tests can run consecutively

--- scripts/smoke.js
+++ scripts/smoke.js
@@ -1,39 +1,50 @@
 const smoke = require("./smoke-test.js");
 
-// smoke("jquery", "dist/jquery.js", {
-//   build: "npm run build",
-//   test: "grunt test",
-//   success: "872 passing",
-// });
+var tests = [
+  {
+    dir: "jquery",
+    file: "dist/jquery.js",
+    build: "npm run build",
+    test: "grunt test",
+    success: "872 passing",
+  },
+  {
+    dir: "html-minifier",
+    file: "src/htmlminifier.js",
+    build: "npm install && grunt dist",
+    test: "grunt qunit",
+    success: "0 failed",
+  },
+  // {
+  //   dir: "immutable-js",
+  //   file: "dist/immutable.js",
+  //   build: "npm install && npm run build",
+  //   test: "npm run testonly",
+  //   babiliOptions: {
+  //     "unsafe": {
+  //       typeConstructors: false,
+  //     },
+  //   },
+  // },
+  // {
+  //   dir: "react",
+  //   file: "build/react.js",
+  //   build: "npm install && npm run build",
+  //   test: "npm test",
+  // },
+  // {
+  //   dir: "draft-js",
+  //   file: "dist/Draft.js",
+  //   build: "npm install && gulp build",
+  //   test: "npm test",
+  // }
+];
 
-// pass
-
-// smoke("immutable-js", "dist/immutable.js", {
-//   build: "npm install && npm run build",
-//   test: "npm run testonly",
-//   babiliOptions: {
-//     "unsafe": {
-//       typeConstructors: false,
-//     },
-//   },
-// });
-
-// pass
-
-// smoke("react", "build/react.js", {
-//   build: "npm install && npm run build",
-//   test: "npm test",
-// });
-
-// // doesn"t use build files?
-
-smoke("draft-js", "dist/Draft.js", {
-  build: "npm install && gulp build",
-  test: "npm test",
-});
-
-// smoke("html-minifier", "src/htmlminifier.js", {
-//   build: "npm install && grunt dist",
-//   test: "grunt qunit",
-//   success: "0 failed",
-// });
+(function tick(test) {
+  smoke(test, (isSuccessful) => {
+    const test = tests.pop();
+    if (isSuccessful && test) {
+      tick(test);
+    }
+  });
+})(tests.pop());
