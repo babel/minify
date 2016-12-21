eaad267365c2f5e2da100b5bf252ea5bced7340d
Juriy Zaytsev <kangax@fb.com>
2016-12-21 12:48:49 -0500

    Fix linter

--- scripts/smoke-test.js
+++ scripts/smoke-test.js
@@ -1,66 +1,66 @@
-const exec = require('child_process').exec;
-const fs = require('fs');
-const transform = require('babel-core').transform;
-const chalk = require('chalk');
+const exec = require("child_process").exec;
+const fs = require("fs");
+const transform = require("babel-core").transform;
+const chalk = require("chalk");
 
 module.exports = function(dirPath, filePath, commands) {
 
-	if (commands.build) {
-		commands.build = `cd ${dirPath} && ${commands.build}`;
-	}
-	commands.test = `cd ${dirPath} && ${commands.test}`;
+  if (commands.build) {
+    commands.build = `cd ${dirPath} && ${commands.build}`;
+  }
+  commands.test = `cd ${dirPath} && ${commands.test}`;
 
-	console.log(chalk.green('1.', commands.build || 'Nothing to build'));
+  console.log(chalk.green("1.", commands.build || "Nothing to build"));
 
-	if (commands.build) {
-		const buildProcess = exec(commands.build, (err, stdout, stderr) => {
-		  if (err) {
-		    console.error(`Error building: ${err}`);
-		    return;
-		  }
-		  minify();
-		});
+  if (commands.build) {
+    const buildProcess = exec(commands.build, (err) => {
+      if (err) {
+        console.error(`Error building: ${err}`);
+        return;
+      }
+      minify();
+    });
 
-		buildProcess.stdout.pipe(process.stdout);
-	}
-	else {
-		minify();
-	}
+    buildProcess.stdout.pipe(process.stdout);
+  }
+  else {
+    minify();
+  }
 
-	function minify() {
-		fs.readFile(`${dirPath}/${filePath}`, (err, data) => {
-	  	if (err) {
-	  	  console.error(`Error reading file: ${err}`);
-	  	  return;
-	  	}
+  function minify() {
+    fs.readFile(`${dirPath}/${filePath}`, (err, data) => {
+      if (err) {
+        console.error(`Error reading file: ${err}`);
+        return;
+      }
 
-	  	console.log(chalk.green('2. Minifying', filePath));
+      console.log(chalk.green("2. Minifying", filePath));
 
-	  	const { code: minified } = transform(data.toString(), {
-	  		comments: false,
-	  		minified: true,
-	  	  passPerPreset: true,
-	  	  presets: ['babili'],
-	  	});
+      const { code: minified } = transform(data.toString(), {
+        comments: false,
+        minified: true,
+        passPerPreset: true,
+        presets: ["babili"],
+      });
 
-	  	fs.writeFile(`${dirPath}/${filePath}`, minified, (err) => {
-	  		if (err) {
-	  			console.error(`Error writing file: ${err}`);
-	  			return;
-	  		}
+      fs.writeFile(`${dirPath}/${filePath}`, minified, (err) => {
+        if (err) {
+          console.error(`Error writing file: ${err}`);
+          return;
+        }
 
-	  		console.log(chalk.green('3.', commands.test));
-	  		const testProcess = exec(commands.test, (err, stdout, stderr) => {
-	  			if (err) {
-	  			  console.error(`Error testing: ${err}`);
-	  			  return;
-	  			}
-	  			console.log(stdout);
-	  			process.exit(0);
-	  		});
+        console.log(chalk.green("3.", commands.test));
+        const testProcess = exec(commands.test, (err, stdout) => {
+          if (err) {
+            console.error(`Error testing: ${err}`);
+            return;
+          }
+          console.log(stdout);
+          process.exit(0);
+        });
 
-	  		testProcess.stdout.pipe(process.stdout);
-	  	});
-	  });
-	}
-}
+        testProcess.stdout.pipe(process.stdout);
+      });
+    });
+  }
+};
--- scripts/smoke.js
+++ scripts/smoke.js
@@ -1,32 +1,32 @@
-const smoke = require('./smoke-test.js');
+const smoke = require("./smoke-test.js");
 
-// smoke('jquery', 'dist/jquery.js', {
-//   build: 'npm run build',
-//   test: 'grunt test',
+// smoke("jquery", "dist/jquery.js", {
+//   build: "npm run build",
+//   test: "grunt test",
 // });
 
 // // pass
 
-// smoke('immutable-js', 'dist/immutable.js', {
-//   build: 'npm install && npm run build',
-//   test: 'npm run testonly',
+// smoke("immutable-js", "dist/immutable.js", {
+//   build: "npm install && npm run build",
+//   test: "npm run testonly",
 // });
 
 // // pass
 
-// smoke('react', 'build/react.js', {
-//   build: 'npm install && npm run build',
-//   test: 'npm test',
+// smoke("react", "build/react.js", {
+//   build: "npm install && npm run build",
+//   test: "npm test",
 // });
 
-// // doesn't use build files?
+// // doesn"t use build files?
 
-// smoke('draft-js', 'dist/Draft.js', {
-//   build: 'npm install && gulp build',
-//   test: 'npm test',
+// smoke("draft-js", "dist/Draft.js", {
+//   build: "npm install && gulp build",
+//   test: "npm test",
 // });
 
-smoke('html-minifier', 'src/htmlminifier.js', {
-	build: 'npm install && grunt dist',
-  test: 'grunt qunit',
+smoke("html-minifier", "src/htmlminifier.js", {
+  build: "npm install && grunt dist",
+  test: "grunt qunit",
 });
