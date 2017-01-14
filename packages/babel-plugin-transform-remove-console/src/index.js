1cc9592155bc075ea16feb352dbd28c7f6d5beff
Andrew Levine <theandrewtizer@gmail.com>
2017-01-13 23:30:58 -0600

    Account for removal of consequent inside ternary for no-console plugin. Fixes #374

--- packages/babel-plugin-transform-remove-console/src/index.js
+++ packages/babel-plugin-transform-remove-console/src/index.js
@@ -1,12 +1,17 @@
 "use strict";
 
-module.exports = function() {
+module.exports = function({ types: t }) {
   return {
     name: "transform-remove-console",
     visitor: {
       CallExpression(path) {
-        if (path.get("callee").matchesPattern("console", true)) {
+        const isConsole = path.get("callee").matchesPattern("console", true);
+        const { parentPath } = path;
+
+        if ((parentPath.isExpressionStatement() || parentPath.isLogicalExpression()) && isConsole) {
           path.remove();
+        } else if (isConsole) {
+          path.replaceWith(t.arrowFunctionExpression([], t.blockStatement([])));
         }
       },
     },
