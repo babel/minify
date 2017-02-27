19d740cfb292164274f9bc048099b8e1bc2adaf4
Boopathi Rajaa <boopathi@users.noreply.github.com>
2017-02-27 14:04:18 +0100

    Deopt do..while stmts with break (#433)
    * Deopt do..while stmts with break - (fix #419)
    
    * Add continue statement

--- packages/babel-plugin-minify-dead-code-elimination/__tests__/dead-code-elimination-test.js
+++ packages/babel-plugin-minify-dead-code-elimination/__tests__/dead-code-elimination-test.js
@@ -2522,4 +2522,157 @@ describe("dce-plugin", () => {
 
     expect(transform(source)).toBe(expected);
   });
+
+  it("should deopt for impure tests", () => {
+    const source = unpad(`
+      function foo() {
+        do {
+          bar();
+        } while ((bar(), false));
+        for (; bar(), false;) {
+          bar();
+        }
+        while (bar(), false) {
+          bar();
+        }
+      }
+    `);
+    expect(transform(source)).toBe(source);
+  });
+
+  it("should handle confident do..while with break statements", () => {
+    const source = unpad(`
+      function foo() {
+        do {
+          if (x) break;
+        } while (false);
+
+        do break; while (false);
+
+        bar0: do break; while (false);
+
+        bar1: do break bar1; while (false);
+
+        bar2: do {
+          if (y) break;
+        } while (false);
+
+        bar3: do {
+          if (y) break bar3;
+        } while (false);
+
+        bar4: do {
+          while (baz()) {
+            if (x) break;
+          }
+        } while (false);
+
+        bar5: do {
+          while (baz()) {
+            if (x) break bar5;
+          }
+        } while (false);
+      }
+    `);
+    const expected = unpad(`
+      function foo() {
+        do {
+          if (x) break;
+        } while (false);
+
+        bar1: do break bar1; while (false);
+
+        bar2: do {
+          if (y) break;
+        } while (false);
+
+        bar3: do {
+          if (y) break bar3;
+        } while (false);
+
+        bar4: {
+          while (baz()) {
+            if (x) break;
+          }
+        }
+
+        bar5: do {
+          while (baz()) {
+            if (x) break bar5;
+          }
+        } while (false);
+      }
+    `);
+    expect(transform(source)).toBe(expected);
+  });
+
+  it("should handle confident do..while with continue statements", () => {
+    const source = unpad(`
+      function foo() {
+        do {
+          if (x) continue;
+        } while (false);
+
+        do continue; while (false);
+
+        bar0: do continue; while (false);
+
+        bar1: do continue bar1; while (false);
+
+        bar2: do {
+          if (y) continue;
+        } while (false);
+
+        bar3: do {
+          if (y) continue bar3;
+        } while (false);
+
+        bar4: do {
+          while (baz()) {
+            if (x) continue;
+          }
+        } while (false);
+
+        bar5: do {
+          while (baz()) {
+            if (x) continue bar5;
+          }
+        } while (false);
+      }
+    `);
+    const expected = unpad(`
+      function foo() {
+        do {
+          if (x) continue;
+        } while (false);
+
+        do continue; while (false);
+
+        bar0: do continue; while (false);
+
+        bar1: do continue bar1; while (false);
+
+        bar2: do {
+          if (y) continue;
+        } while (false);
+
+        bar3: do {
+          if (y) continue bar3;
+        } while (false);
+
+        bar4: {
+          while (baz()) {
+            if (x) continue;
+          }
+        }
+
+        bar5: do {
+          while (baz()) {
+            if (x) continue bar5;
+          }
+        } while (false);
+      }
+    `);
+    expect(transform(source)).toBe(expected);
+  });
 });
