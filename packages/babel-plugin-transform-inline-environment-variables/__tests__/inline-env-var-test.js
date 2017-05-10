c1e83902257b85e612b1b5cee814c05b88651bf0
Jake Dluhy <dluhy.jake@gmail.com>
2017-05-10 12:05:23 -0700

    Add whitelist/blacklist to transform-inline-environment-variables

--- packages/babel-plugin-transform-inline-environment-variables/__tests__/inline-env-var-test.js
+++ packages/babel-plugin-transform-inline-environment-variables/__tests__/inline-env-var-test.js
@@ -1,14 +1,16 @@
 jest.autoMockOff();
 
-const thePlugin = require("../../../utils/test-transform")(
-  require("../src/index")
-);
+const plugin = require("../src/index")
+const thePlugin = require("../../../utils/test-transform")(plugin);
 
 describe("inline-env-plugin", () => {
   let prev;
   beforeAll(() => {
     prev = process.env.NODE_ENV;
     process.env.NODE_ENV = "development";
+  });
+  afterAll(() => {
+    process.env.NODE_ENV = prev;
   });
 
   thePlugin(
@@ -21,7 +23,21 @@ describe("inline-env-plugin", () => {
   `
   );
 
-  afterAll(() => {
-    process.env.NODE_ENV = prev;
-  });
+  thePlugin(
+    "should only include whitelisted variables if include option is specified",
+    "process.env.NODE_ENV",
+    "process.env.NODE_ENV;",
+    {
+      plugins: [[plugin, { include: ["IS_ELECTRON"] }]]
+    }
+  );
+
+  thePlugin(
+    "should not include blacklisted variables if exclude option is specified",
+    "process.env.NODE_ENV",
+    "process.env.NODE_ENV;",
+    {
+      plugins: [[plugin, { exclude: ["NODE_ENV"] }]]
+    }
+  );
 });
--- packages/babel-plugin-transform-inline-environment-variables/src/index.js
+++ packages/babel-plugin-transform-inline-environment-variables/src/index.js
@@ -4,10 +4,14 @@ module.exports = function({ types: t }) {
   return {
     name: "transform-inline-environment-variables",
     visitor: {
-      MemberExpression(path) {
+      MemberExpression(path, { opts: { include, exclude } = {} }) {
         if (path.get("object").matchesPattern("process.env")) {
           const key = path.toComputedKey();
-          if (t.isStringLiteral(key)) {
+          if (
+            t.isStringLiteral(key) &&
+            (!include || include.includes(key.value)) &&
+            (!exclude || !exclude.includes(key.value))
+          ) {
             path.replaceWith(t.valueToNode(process.env[key.value]));
           }
         }
