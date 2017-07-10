d21a7180baafdd0a817a6096f1b2db4e0d721fbe
Boopathi Rajaa <me@boopathi.in>
2017-07-10 14:25:21 +0200

    Move AssignmentExpression to separate file

--- packages/babel-plugin-minify-simplify/src/index.js
+++ packages/babel-plugin-minify-simplify/src/index.js
@@ -8,72 +8,12 @@ module.exports = ({ types: t }) => {
   const ifStatement = require("./if-statement")(t);
   const conditionalExpression = require("./conditional-expression")(t);
   const logicalExpression = require("./logical-expression")(t);
+  const assignmentExpression = require("./assignment-expression")(t);
 
   const VOID_0 = t.unaryExpression("void", t.numericLiteral(0), true);
   const condExprSeen = Symbol("condExprSeen");
   const seqExprSeen = Symbol("seqExprSeen");
   const shouldRevisit = Symbol("shouldRevisit");
-
-  const operators = new Set([
-    "+",
-    "-",
-    "*",
-    "%",
-    "<<",
-    ">>",
-    ">>>",
-    "&",
-    "|",
-    "^",
-    "/",
-    "**"
-  ]);
-
-  const updateOperators = new Set(["+", "-"]);
-
-  function areArraysEqual(arr1, arr2) {
-    return arr1.every((value, index) => {
-      return String(value) === String(arr2[index]);
-    });
-  }
-
-  function getName(node) {
-    if (node.type === "ThisExpression") {
-      return "this";
-    }
-    if (node.type === "Super") {
-      return "super";
-    }
-    if (node.type === "NullLiteral") {
-      return "null";
-    }
-    // augment identifiers so that they don't match
-    // string/number literals
-    // but still match against each other
-    return node.name ? node.name + "_" : node.value /* Literal */;
-  }
-
-  function getPropNames(path) {
-    if (!path.isMemberExpression()) {
-      return;
-    }
-
-    let obj = path.get("object");
-
-    const prop = path.get("property");
-    const propNames = [getName(prop.node)];
-
-    while (obj.type === "MemberExpression") {
-      const node = obj.get("property").node;
-      if (node) {
-        propNames.push(getName(node));
-      }
-      obj = obj.get("object");
-    }
-    propNames.push(getName(obj.node));
-
-    return propNames;
-  }
 
   return {
     name: "minify-simplify",
@@ -183,62 +123,7 @@ module.exports = ({ types: t }) => {
         exit: logicalExpression.simplifyPatterns
       },
 
-      AssignmentExpression(path) {
-        const rightExpr = path.get("right");
-        const leftExpr = path.get("left");
-
-        if (path.node.operator !== "=") {
-          return;
-        }
-
-        const canBeUpdateExpression =
-          rightExpr.get("right").isNumericLiteral() &&
-          rightExpr.get("right").node.value === 1 &&
-          updateOperators.has(rightExpr.node.operator);
-
-        if (leftExpr.isMemberExpression()) {
-          const leftPropNames = getPropNames(leftExpr);
-          const rightPropNames = getPropNames(rightExpr.get("left"));
-
-          if (
-            !leftPropNames ||
-            leftPropNames.indexOf(undefined) > -1 ||
-            !rightPropNames ||
-            rightPropNames.indexOf(undefined) > -1 ||
-            !operators.has(rightExpr.node.operator) ||
-            !areArraysEqual(leftPropNames, rightPropNames)
-          ) {
-            return;
-          }
-        } else {
-          if (
-            !rightExpr.isBinaryExpression() ||
-            !operators.has(rightExpr.node.operator) ||
-            leftExpr.node.name !== rightExpr.node.left.name
-          ) {
-            return;
-          }
-        }
-
-        let newExpression;
-
-        // special case x=x+1 --> ++x
-        if (canBeUpdateExpression) {
-          newExpression = t.updateExpression(
-            rightExpr.node.operator + rightExpr.node.operator,
-            t.clone(leftExpr.node),
-            true /* prefix */
-          );
-        } else {
-          newExpression = t.assignmentExpression(
-            rightExpr.node.operator + "=",
-            t.clone(leftExpr.node),
-            t.clone(rightExpr.node.right)
-          );
-        }
-
-        path.replaceWith(newExpression);
-      },
+      AssignmentExpression: assignmentExpression.simplify,
 
       ConditionalExpression: {
         enter: [
