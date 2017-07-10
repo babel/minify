25e57f246a41bdb8b33bfe969016583945801d7b
Boopathi Rajaa <boopathi@users.noreply.github.com>
2017-07-10 19:57:42 +0200

    Split simplify plugin to multiple files (#624)
    * Split Simplify to multiple files - IfStatement
    
    * Extract Conditional Expression transformations to separate file
    
    * Move Logical Expression patterns to separate file
    
    * Move AssignmentExpression to separate file

---
+++ packages/babel-plugin-minify-simplify/src/conditional-expression.js
@@ -0,0 +1,53 @@
+const h = require("./helpers");
+const PatternMatch = require("./pattern-match");
+
+module.exports = t => {
+  // small abstractions
+  const not = node => t.unaryExpression("!", node);
+  const notnot = node => not(not(node));
+  const or = (a, b) => t.logicalExpression("||", a, b);
+  const and = (a, b) => t.logicalExpression("&&", a, b);
+
+  function simplifyPatterns(path) {
+    const test = path.get("test");
+    const consequent = path.get("consequent");
+    const alternate = path.get("alternate");
+
+    const { Expression: EX, LogicalExpression: LE } = h.typeSymbols(t);
+
+    // Convention:
+    // ===============
+    // for each pattern [test, consequent, alternate, handler(expr, cons, alt)]
+    const matcher = new PatternMatch([
+      [LE, true, false, e => e],
+      [EX, true, false, e => notnot(e)],
+
+      [EX, false, true, e => not(e)],
+
+      [LE, true, EX, (e, c, a) => or(e, a)],
+      [EX, true, EX, (e, c, a) => or(notnot(e), a)],
+
+      [EX, false, EX, (e, c, a) => and(not(e), a)],
+
+      [EX, EX, true, (e, c) => or(not(e), c)],
+
+      [LE, EX, false, (e, c) => and(e, c)],
+      [EX, EX, false, (e, c) => and(notnot(e), c)]
+    ]);
+
+    const result = matcher.match(
+      [test, consequent, alternate],
+      h.isPatternMatchesPath(t)
+    );
+
+    if (result.match) {
+      path.replaceWith(
+        result.value(test.node, consequent.node, alternate.node)
+      );
+    }
+  }
+
+  return {
+    simplifyPatterns
+  };
+};
---
+++ packages/babel-plugin-minify-simplify/src/logical-expression.js
@@ -0,0 +1,79 @@
+"use strict";
+
+const h = require("./helpers");
+const PatternMatch = require("./pattern-match");
+
+module.exports = t => {
+  const OP_AND = input => input === "&&";
+  const OP_OR = input => input === "||";
+
+  function simplifyPatterns(path) {
+    // cache of path.evaluate()
+    const evaluateMemo = new Map();
+
+    const TRUTHY = input => {
+      // !NaN and !undefined are truthy
+      // separate check here as they are considered impure by babel
+      if (input.isUnaryExpression() && input.get("argument").isIdentifier()) {
+        if (
+          input.node.argument.name === "NaN" ||
+          input.node.argument.name === "undefined"
+        ) {
+          return true;
+        }
+      }
+      const evalResult = input.evaluate();
+      evaluateMemo.set(input, evalResult);
+      return evalResult.confident && input.isPure() && evalResult.value;
+    };
+
+    const FALSY = input => {
+      // NaN and undefined are falsy
+      // separate check here as they are considered impure by babel
+      if (input.isIdentifier()) {
+        if (input.node.name === "NaN" || input.node.name === "undefined") {
+          return true;
+        }
+      }
+      const evalResult = input.evaluate();
+      evaluateMemo.set(input, evalResult);
+      return evalResult.confident && input.isPure() && !evalResult.value;
+    };
+
+    const { Expression: EX } = h.typeSymbols(t);
+
+    // Convention:
+    // [left, operator, right, handler(leftNode, rightNode)]
+    const matcher = new PatternMatch([
+      [TRUTHY, OP_AND, EX, (l, r) => r],
+      [FALSY, OP_AND, EX, l => l],
+      [TRUTHY, OP_OR, EX, l => l],
+      [FALSY, OP_OR, EX, (l, r) => r]
+    ]);
+
+    const left = path.get("left");
+    const right = path.get("right");
+    const operator = path.node.operator;
+
+    const result = matcher.match(
+      [left, operator, right],
+      h.isPatternMatchesPath(t)
+    );
+
+    if (result.match) {
+      // here we are sure that left.evaluate is always confident becuase
+      // it satisfied one of TRUTHY/FALSY paths
+      let value;
+      if (evaluateMemo.has(left)) {
+        value = evaluateMemo.get(left).value;
+      } else {
+        value = left.evaluate().value;
+      }
+      path.replaceWith(result.value(t.valueToNode(value), right.node));
+    }
+  }
+
+  return {
+    simplifyPatterns
+  };
+};
