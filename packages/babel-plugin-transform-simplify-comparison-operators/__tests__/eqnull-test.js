53898344828f85eb96d6e13641499c9df148b4e5
Jed Fox <git@twopointzero.us>
2017-08-17 07:48:15 -0400

    thePlugin

--- packages/babel-plugin-transform-simplify-comparison-operators/__tests__/eqnull-test.js
+++ packages/babel-plugin-transform-simplify-comparison-operators/__tests__/eqnull-test.js
@@ -1,29 +1,23 @@
 jest.autoMockOff();
 
-const babel = require("babel-core");
-const plugin = require("../src/index");
-
-function transform(code) {
-  return babel.transform(code, {
-    plugins: [plugin]
-  }).code;
-}
+const thePlugin = require("../../../utils/test-transform")(
+  require("../src/index")
+);
 
 describe("simplify-comparison-operators-plugin", () => {
-  it("should shorten `x == undefined`", () => {
-    const source = "x == undefined;";
-    const expected = "x == null;";
-    expect(transform(source)).toBe(expected);
-  });
+  thePlugin("should shorten `x == undefined`", `
+    x == undefined;
+  `, `
+    x == null;
+  `);
 
-  it("should shorten `x == void 0`", () => {
-    const source = "x == void 0;";
-    const expected = "x == null;";
-    expect(transform(source)).toBe(expected);
-  });
+  thePlugin("should shorten `x == void 0`", `
+    x == void 0;
+  `, `
+    x == null;
+  `);
 
-  it("should not shorten `x === undefined`", () => {
-    const source = "x === undefined;";
-    expect(transform(source)).toBe(source);
-  });
+  thePlugin("should not shorten `x === undefined`", `
+    x === undefined;
+  `);
 });
