9112f68e5bdde2300dae3807dd20389458b0a41f
Mauro Bringolf <mauro@webkinder.ch>
2018-02-02 11:59:57 +0100

    Add example where hexadecimal is shorter

--- packages/babel-plugin-minify-numeric-literals/src/index.js
+++ packages/babel-plugin-minify-numeric-literals/src/index.js
@@ -9,6 +9,7 @@ module.exports = function() {
 
         const normal = path.node.value.toString().replace(/^0\./, ".");
         let exponential = path.node.value.toExponential().replace(/\+/g, "");
+        const hexadecimal = `0x${path.node.value.toString(16)}`
 
         if (exponential.indexOf(".") >= 0 && exponential.indexOf("e") >= 0) {
           const lastChar = exponential.substr(exponential.lastIndexOf("e") + 1);
@@ -25,8 +26,10 @@ module.exports = function() {
             .replace(/e0/, "");
         }
 
-        const replacement =
+        let replacement =
           normal.length > exponential.length ? exponential : normal;
+
+        replacement = replacement.length > hexadecimal.length ? hexadecimal : replacement;
 
         if (path.node.extra.raw.length > replacement.length) {
           path.node.extra.raw = replacement;
