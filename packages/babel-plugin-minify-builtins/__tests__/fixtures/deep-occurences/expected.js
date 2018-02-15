9a196d995289eec71ea9c8042cc3f5646ceffab0
Boopathi Rajaa <me@boopathi.in>
2018-02-15 23:11:00 +0100

    fix: formatting minify-builtins

--- packages/babel-plugin-minify-builtins/__tests__/fixtures/deep-occurences/expected.js
+++ packages/babel-plugin-minify-builtins/__tests__/fixtures/deep-occurences/expected.js
@@ -1,11 +1,14 @@
 function a() {
   Math.max(c, a);
+
   const b = () => {
     var _Mathmin = Math.min;
     var _Mathfloor = Math.floor;
 
     const a = _Mathfloor(c);
+
     _Mathmin(b, a) * _Mathfloor(b);
+
     function c() {
       _Mathfloor(c) + _Mathmin(b, a);
     }
--- packages/babel-plugin-minify-builtins/__tests__/fixtures/in-function-scope/expected.js
+++ packages/babel-plugin-minify-builtins/__tests__/fixtures/in-function-scope/expected.js
@@ -2,21 +2,24 @@ var a = () => {
   var _Mathfloor = Math.floor;
 
   _Mathfloor(b);
+
   _Mathfloor(b);
+
   c: () => {
     _Mathfloor(d);
+
     2;
   };
 };
+
 A.b("asdas", function () {
   var _Mathmax = Math.max;
-
   Math.floor(d) + _Mathmax(d, e);
+
   _Mathmax(e, d);
 });
 A.b("asdas1", function () {
   var _Mathfloor2 = Math.floor;
-
   _Mathfloor2(d) + _Mathfloor2(d, e);
   Math.max(e, d);
 });
