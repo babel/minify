07712abedbd58fc196add77f5cd82a34cc1d9d63
Boopathi Rajaa <me@boopathi.in>
2018-02-20 02:09:12 +0100

    fix: formatting of minify-type-constructors

--- packages/babel-plugin-minify-type-constructors/__tests__/fixtures/object/expected.js
+++ packages/babel-plugin-minify-type-constructors/__tests__/fixtures/object/expected.js
@@ -1,18 +1,20 @@
 var x = {};
+var x = {}; // TODO: add Object(Array())
 
-var x = {};
+[{}, {}, {}]; // TODO: add Object(Array())
 
-// TODO: add Object(Array())
-[{}, {}, {}];
-
-// TODO: add Object(Array())
-[{}, { a: b }, { a: b, c: d }];
-
+[{}, {
+  a: b
+}, {
+  a: b,
+  c: d
+}];
 [[], [1], [1, 2], [null]];
 
 function a() {}
+
 [function () {}, a, Object(Array)];
-
 [Object("undefined"), Object(nulled), Object(0), Object(false), Object(stuff())];
-
-[Object("function"), Object(Symbol), Object(true), Object(1), Object(call({ me: true }))];
+[Object("function"), Object(Symbol), Object(true), Object(1), Object(call({
+  me: true
+}))];
