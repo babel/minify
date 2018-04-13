559e836a50eed2e1a3047bcb2b49ce990133ecb2
Jed Fox <git@twopointzero.us>
2018-04-13 11:01:54 -0400

    Update negated-equal fixture

--- packages/babel-plugin-minify-simplify/__tests__/fixtures/negated-equal/expected.js
+++ packages/babel-plugin-minify-simplify/__tests__/fixtures/negated-equal/expected.js
@@ -1,6 +1,7 @@
 if (foo !== 2) {
   const thisIs = aStatement;
 }
+
 if (foo === 2) {
   const thisIs = aStatement;
 }
@@ -8,24 +9,81 @@ if (foo === 2) {
 if (foo ^ 2) {
   const thisIs = aStatement;
 }
+
 if (foo == 2) {
   const thisIs = aStatement;
 }
 
-if (myFoo({ bar: { baz: { quux() {} } } }) !== 2) {
+if (myFoo({
+  bar: {
+    baz: {
+      quux() {}
+
+    }
+  }
+}) !== 2) {
   const thisIs = aStatement;
 }
-if (myFoo({ bar: { baz: { quux() {} } } }) === 2) {
+if (myFoo({
+  bar: {
+    baz: {
+      quux() {}
+
+    }
+  }
+}) === 2) {
   const thisIs = aStatement;
 }
 
-if (myFoo({ bar: { baz: { quux() {} } } }) ^ 2) {
+if (myFoo({
+  bar: {
+    baz: {
+      quux() {}
+
+    }
+  }
+}) ^ 2) {
   const thisIs = aStatement;
 }
-if (myFoo({ bar: { baz: { quux() {} } } }) == 2) {
+if (myFoo({
+  bar: {
+    baz: {
+      quux() {}
+
+    }
+  }
+}) == 2) {
   const thisIs = aStatement;
 }
 
 // --------------------------------------
 
-foo !== 2 && an.expression(), foo === 2 && an.expression(), foo ^ 2 && an.expression(), foo == 2 && an.expression(), myFoo({ bar: { baz: { quux() {} } } }) !== 2 && an.expression(), myFoo({ bar: { baz: { quux() {} } } }) === 2 && an.expression(), myFoo({ bar: { baz: { quux() {} } } }) ^ 2 && an.expression(), myFoo({ bar: { baz: { quux() {} } } }) == 2 && an.expression();
+foo !== 2 && an.expression(), foo === 2 && an.expression(), foo ^ 2 && an.expression(), foo == 2 && an.expression(), myFoo({
+  bar: {
+    baz: {
+      quux() {}
+
+    }
+  }
+}) !== 2 && an.expression(), myFoo({
+  bar: {
+    baz: {
+      quux() {}
+
+    }
+  }
+}) === 2 && an.expression(), myFoo({
+  bar: {
+    baz: {
+      quux() {}
+
+    }
+  }
+}) ^ 2 && an.expression(), myFoo({
+  bar: {
+    baz: {
+      quux() {}
+
+    }
+  }
+}) == 2 && an.expression();
