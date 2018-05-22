a28b6b000a05934864db9aea70cef671ea79b167
Boopathi Rajaa <boopathi@users.noreply.github.com>
2018-05-22 16:36:19 +0300

    feat(cli): add sourceType and comments (#861)

---
+++ packages/babel-minify/__tests__/fixtures/module/mod.js
@@ -0,0 +1,16 @@
+// comment 1
+import Foo from "foo";
+
+// comment 2
+import pick from "lodash.pick";
+
+export const items /* comment 3 */ = pick(Foo.all, [
+  // comment 4
+  "a",
+  // comment 5
+  "b",
+  // comment 6
+  "c"
+]);
+
+export default Foo(items);
