cda70da04a64a4311eaf8c9b3fb2333539dec096
Yukimasa Funaoka <yukimasafunaoka@gmail.com>
2020-08-14 02:12:51 +0900

    fix: an error occurred if includes environment variable assignment expression (#970)

--- packages/babel-plugin-transform-inline-environment-variables/src/index.js
+++ packages/babel-plugin-transform-inline-environment-variables/src/index.js
@@ -1,6 +1,11 @@
 "use strict";
 
 module.exports = function({ types: t }) {
+  function isLeftSideOfAssignmentExpression(path) {
+    return (
+      t.isAssignmentExpression(path.parent) && path.parent.left === path.node
+    );
+  }
   return {
     name: "transform-inline-environment-variables",
     visitor: {
@@ -9,6 +14,7 @@ module.exports = function({ types: t }) {
           const key = path.toComputedKey();
           if (
             t.isStringLiteral(key) &&
+            !isLeftSideOfAssignmentExpression(path) &&
             (!include || include.indexOf(key.value) !== -1) &&
             (!exclude || exclude.indexOf(key.value) === -1)
           ) {
