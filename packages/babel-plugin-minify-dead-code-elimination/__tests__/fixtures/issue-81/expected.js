749f7afe016b60e1ea7e40cc94e40206df4d1511
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-05-06 19:57:39 +0200

    chore: update failing fixture

--- packages/babel-plugin-minify-dead-code-elimination/__tests__/fixtures/issue-81/expected.js
+++ packages/babel-plugin-minify-dead-code-elimination/__tests__/fixtures/issue-81/expected.js
@@ -1,7 +1,9 @@
 (function (require, module) {
-  module.exports = function Hub() {
+  var Hub = function Hub() {
     (0, _classCallCheck3.default)(this, Hub);
   };
+
+  module.exports = Hub;
 })(require, module, exports);
 
 (function () {
